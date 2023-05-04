24b219c7825ae039918013ea9f8ae855d95f4a50
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-05-04 11:53:23 +0530

    Added solution - LeetHub

---
+++ Sum of Right Leaf Nodes - GFG/sum-of-right-leaf-nodes.cpp
@@ -0,0 +1,151 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+        return;
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/*Structure of the node of the binary tree is as
+
+
+/*Structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+*/
+// function should return the sum of all 
+// right leaf nodes
+class Solution{
+    public:
+    void solve(Node* root, Node* parent, bool isRight, int &sum){
+        if(root==NULL){
+          return ;
+
+        }
+        solve(root->left, root, false, sum);
+        solve(root->right, root, true, sum);
+        if(root->left == NULL && root->right == NULL && isRight){
+            sum += root->data;
+        }
+    }
+    int rightLeafSum(Node* root)
+    {
+        int sum = 0 ;
+        solve(root, NULL, false, sum);
+        return sum ;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        Node *root = buildTree(s);
+        Solution obj;
+        int ans = obj.rightLeafSum(root);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
+
+
+
+// } Driver Code Ends
