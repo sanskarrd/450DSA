3794204d53d77fb62b7c317a72076fec67938ed5
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-05-29 13:04:07 +0530

    Added solution - LeetHub

---
+++ Check set bits - GFG/check-set-bits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+   
+class Solution{
+public:
+    int isBitSet(int n){
+        // code here 
+        if (n==0) return false ;
+        if((n&n+1)==0)
+        return true ;
+        return false ;
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.isBitSet(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
