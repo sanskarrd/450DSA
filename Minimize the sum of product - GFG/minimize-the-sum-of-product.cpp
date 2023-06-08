7ad6741e3629974577ca6312c4dc69334825fe7b
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-08 07:51:03 +0530

    Added solution - LeetHub

---
+++ Minimize the sum of product - GFG/minimize-the-sum-of-product.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long int minValue(int a[], int b[], int n)
+    {
+        // Your code goes here
+        sort(a,a+n) ;
+        sort(b,b+n) ;
+        reverse(b,b+n) ;
+        
+        long long int minPro = 0;
+        for(int i = 0 ; i<n ; i++){
+            minPro += a[i]*b[i] ;
+        }
+        return minPro ;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n, i;
+         cin>>n;
+         int a[n], b[n];
+         for(i=0;i<n;i++)
+         cin>>a[i];
+         for(i=0;i<n;i++)
+         cin>>b[i];
+         Solution ob;
+         cout<< ob.minValue(a, b, n) <<endl;
+     }
+	
+	return 0;
+}
+// } Driver Code Ends
