c4fc16790ddb3f0d1b325f0a26adc6cbaa283555
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-14 09:23:15 +0530

    Added solution - LeetHub

--- Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
@@ -8,30 +8,29 @@ class Solution{
     public:
     //Function to find if there exists a triplet in the 
     //array A[] which sums up to X.
-    bool find3Numbers(int A[], int n, int X)
+    bool find3Numbers(int arr[], int n, int X)
     {
-      
-    sort(A,A+n) ;
-    for(int i = 0 ; i<n-2 ; i++)
-    {
-       int x = i+1;
-       int y = n-1;
-       while(x<y)
-       {
-           if(A[i] + A[x] +A[y] == X){
-           return true ;}
+        sort(arr,arr+n);
+        for(int i= 0 ; i <n-2 ; i++){
+            int j = i+1 ;
+            int k = n-1 ;
+            while(j<k){
+                 if(arr[i]+arr[j]+arr[k] == X){
+                return true ;
+            }
+            
+            else if(arr[i]+arr[j]+arr[k] < X){
+                j++ ;
+            }
+            else{
+                k-- ;
+            }
+            }
            
-           else if(A[i] + A[x] + A[y] < X)
-          x++ ;
-           else 
-           y-- ;
-       }
-     
+        }
+        
+        return false ;
     }
-    return false ;
-    
-    }
-       
 
 };
 
