72ccaa99f2787b8f2909aecde5fc6b4b10d0f2e3
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-20 17:59:50 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -8,55 +8,50 @@ class Solution
     public:
     //Function to find minimum time required to rot all oranges. 
     int orangesRotting(vector<vector<int>>& grid) {
-        // Code here
-        int row = grid.size() ;
-        int col = grid[0].size() ;
+        int n = grid.size();
+        int m = grid[0].size();
         
-        int vis[row][col] ;
-        int cntFresh = 0 ;
-        queue<pair<pair<int,int> , int > > q ;
-        for(int i = 0 ; i<row ; i++){
-            for(int j = 0 ; j <col ; j++){
-                
-                if(grid[i][j] == 2){
-                    q.push({{i,j} ,0}) ;
-                    vis[i][j] = 2 ;
+        queue<pair<pair<int,int>,int>> q ;
+        int vis[n][m] ;
+        for(int i = 0 ;i< n ; i++){
+            for(int j = 0 ; j<m ; j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0}) ;
+                    vis[i][j] =2 ;
                 }
                 else{
                     vis[i][j] = 0 ;
                 }
-                if(grid[i][j] ==1)
-                cntFresh++ ;
             }
         }
-        int ans = 0 ;
+        int Time = 0 ;
         int drow[] = {-1,0,1,0} ;
         int dcol[] = {0,1,0,-1} ;
-        int cnt = 0 ;
         while(!q.empty()){
-            int i = q.front().first.first ;
-            int j = q.front().first.second ;
-            int tm = q.front().second ;
-            ans = max(ans,tm) ;
+            int a = q.front().first.first ;
+            int b = q. front().first.second ;
+            int tme =  q.front().second ;
+            Time = max(Time,tme) ;
             q.pop();
-            
-            for(int k = 0 ; k< 4 ;k++ ){
-                int nrow = i+drow[k] ;
-                int ncol = j+dcol[k] ;
-                
-                if(nrow>=0 && nrow<row && ncol>=0 && ncol<col 
-                && vis[nrow][ncol] != 2 && grid[nrow][ncol] ==1){
-                    q.push({{nrow,ncol},tm+1}) ;
+            for(int i = 0 ; i< 4 ; i++){
+                int nrow = a + drow[i] ;
+                int ncol = b + dcol[i] ;
+                if(nrow >=0 && nrow< n && ncol>=0 && ncol< m && vis[nrow][ncol]!=2 && grid[nrow][ncol] ==1){
+                    q.push({{nrow,ncol},tme+1}) ;
                     vis[nrow][ncol] = 2 ;
-                    cnt++ ;
                 }
             }
         }
-        if(cnt!=cntFresh){
-            return -1 ;
+        
+        for(int i = 0 ; i< n ; i++){
+            for(int j = 0 ; j< m; j++){
+                if(vis[i][j]!=2 && grid[i][j]==1){
+                    return -1 ;
+                }
+            }
         }
-      
-        return ans ;
+        
+        return Time ;
     }
 };
 
