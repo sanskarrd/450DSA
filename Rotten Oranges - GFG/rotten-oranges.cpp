6b75b5ccaee3c011b0af02d929a1ec6f23722fab
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-20 18:00:24 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.cpp
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -45,7 +45,7 @@ class Solution
         
         for(int i = 0 ; i< n ; i++){
             for(int j = 0 ; j< m; j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
+                if(vis[i][j]==0 && grid[i][j]==1){
                     return -1 ;
                 }
             }
