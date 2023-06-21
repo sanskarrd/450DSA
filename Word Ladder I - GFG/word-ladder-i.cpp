c155af4e0277b19bc69be58c0d79996957141ef4
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-21 17:40:26 +0530

    Added solution - LeetHub

--- Word Ladder I - GFG/word-ladder-i.cpp
+++ Word Ladder I - GFG/word-ladder-i.cpp
@@ -6,36 +6,36 @@ using namespace std;
 class Solution {
 public:
     int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
-        queue<pair<string,int>>q ;
         
-        unordered_set<string>set(wordList.begin(),wordList.end()) ;
-        
+        queue<pair<string,int> > q ;
+        unordered_set<string> set(wordList.begin(),wordList.end()) ;
         q.push({startWord,1}) ;
+        
+        
         while(!q.empty()){
             string word = q.front().first ;
             int ans  = q.front().second ;
             q.pop() ;
             
-            if(word== targetWord){
-                return ans ;
+            if (word == targetWord){
+                return ans  ;
             }
             
             
-            for(int i = 0 ; i<word.size() ; i++){
-                char chOfStart = word[i] ;
-                for(char ch ='a' ; ch <='z' ; ch++){
-                    word[i]= ch ;
-                    if(set.find(word) !=set.end()){
-                        set.erase(word) ;
-                        q.push({word,ans+1}) ;
-                    }
-                    
-                }
-                word[i]= chOfStart ;
-            }
+           for(int i = 0 ;i<word.size() ; i++){
+               char letter = word[i] ;
+               for(char ch = 'a' ;ch<='z' ; ch++){
+                   word[i] = ch ;
+                   if(set.find(word) != set.end()){
+                       set.erase(word) ;
+                       q.push({word,ans+1}) ;
+                   }
+               }
+               word[i] = letter ;
+           }
         }
-        return 0 ;
         
+        return 0 ;
     }
 };
 
