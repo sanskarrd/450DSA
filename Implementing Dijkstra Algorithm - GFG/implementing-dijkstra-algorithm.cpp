c48424f6c67d82cd72a7e92c8c575be8386dc6f7
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-26 20:43:11 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -6,31 +6,34 @@ using namespace std;
 class Solution
 {
 	public:
-	//Function to find the shortest distance of all the vertices
-    //from the source vertex S.
+
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-         priority_queue<pair<int,int> ,vector<pair<int,int> > ,greater<pair<int,int>> > pq ;
-         vector<int>dis(V,1e9) ;
-         dis[S]=0 ;
-         pq.push({0,S}) ;
-         
-         while(!pq.empty()){
-             int dist = pq.top().first ;
-             int node = pq.top().second ;
-             pq.pop() ;
-             
-             for(auto it: adj[node]){
-                 int edgeWeight = it[1] ;
-                 int adjNode = it[0] ;
-                 if(edgeWeight+dist< dis[adjNode]){
-                     dis[adjNode]= edgeWeight+dist ;
-                     pq.push({dis[adjNode] , adjNode}) ;
-                 }
-             }
-             
-         }
-         return dis ;
+        set<pair<int,int>> st ;
+        vector<int>dis(V,1e9) ;
+        
+        st.insert({0,S}) ;
+        dis[S] = 0 ;
+        
+        while(!st.empty()){
+            auto it = *(st.begin()) ;
+            int node = it.second ;
+            int dist = it.first ;
+            st.erase(it) ;
+            
+            for(auto it : adj[node]){
+            int adjNode = it[0] ;
+            int edgeW = it[1] ;
+            
+            if(dist + edgeW < dis[adjNode]){
+                if(dis[adjNode]!=1e9)
+                st.erase({dis[adjNode],adjNode}) ;
+                dis[adjNode] = dist + edgeW ;
+                st.insert({dis[adjNode] , adjNode}) ;
+            }
+        }
+        }
+        return dis ;
     }
 };
 
