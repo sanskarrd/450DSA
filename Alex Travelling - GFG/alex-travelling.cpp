2cdfc3ba4afbb833a718744b684222f92326c1c1
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-06-27 22:46:33 +0530

    Added solution - LeetHub

--- Alex Travelling - GFG/alex-travelling.cpp
+++ Alex Travelling - GFG/alex-travelling.cpp
@@ -8,51 +8,47 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
- int minimumCost(std::vector<std::vector<int>>& flights, int n, int k) {
-    using namespace std; // Add the using namespace directive
+    int minimumCost(vector<vector<int>>& flights, int n, int k) {
+        vector<pair<int,pair<int,int>>>adj[n+1] ;
+        for(auto it: flights){
+            adj[it[0]].push_back(make_pair(it[1], make_pair(it[2], 0)));
+        }
+        
+        vector<int>dis(n+1,1e9) ;
+        dis[k]= 0;
+       priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+       pq.push(make_pair(0, k));
 
-    vector<pair<int, pair<int, int>>> adj[n + 1]; // Adjusted for city indices 1 to n
-
-    for (auto it : flights) {
-        adj[it[0]].push_back(make_pair(it[1], make_pair(it[2], 0)));
-    }
-
-    vector<int> dis(n + 1, INT_MAX); // Adjusted for city indices 1 to n
-    dis[k] = 0;
-
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push(make_pair(0, k));
-
-    while (!pq.empty()) {
-        int node = pq.top().second;
-        int cost = pq.top().first;
-        pq.pop();
-
-        for (auto it : adj[node]) {
-            int adjN = it.first;
-            int adjC = it.second.first;
-
-            if (cost + adjC < dis[adjN]) {
-                dis[adjN] = cost + adjC;
-                pq.push(make_pair(cost + adjC, adjN));
+        
+        while(!pq.empty())
+        {
+            int node = pq.top().second ;
+            int cost = pq.top().first ;
+            pq.pop() ;
+            
+            for(auto it:adj[node]){
+                int adjN = it.first ;
+                int adjC = it.second.first ;
+                
+                if(cost+adjC<dis[adjN]){
+                    dis[adjN]= cost+adjC ;
+                   pq.push(make_pair(cost + adjC, adjN));
+                }
             }
         }
+        
+       for(int i = 1 ;i<= n ; i++){
+           if(dis[i]  == 1e9){
+               return -1 ;
+           }
+       }
+       int ans = INT_MIN ;
+       for(int i = 1 ; i<= n ; i++){
+           ans  = max(ans,dis[i]) ;
+       }
+       
+       return ans;
     }
-
-    for (int i = 1; i <= n; i++) { // Adjusted for city indices 1 to n
-        if (dis[i] == INT_MAX) {
-            return -1;
-        }
-    }
-
-    int ans = INT_MIN; // Adjusted to initialize with a minimum value
-    for (int i = 1; i <= n; i++) { // Adjusted for city indices 1 to n
-        ans = max(ans, dis[i]);
-    }
-
-    return ans;
-}
-
 };
 
 
