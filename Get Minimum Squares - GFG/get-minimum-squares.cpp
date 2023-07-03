5bfe455e5911f7a4451adde67014eb2df6b6d551
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-07-03 22:13:44 +0530

    Added solution - LeetHub

--- Get Minimum Squares - GFG/get-minimum-squares.cpp
+++ Get Minimum Squares - GFG/get-minimum-squares.cpp
@@ -20,24 +20,34 @@ class Solution{
 // 	    }
 // 	    return ans ;
 // 	}
-	int solveMemoisation(int n,vector<int>&dp){
-	      if(n==0){
-	        return 0 ;
+// 	int solveMemoisation(int n,vector<int>&dp){
+// 	      if(n==0){
+// 	        return 0 ;
+// 	    }
+// 	    if(dp[n]!=-1){
+// 	        return dp[n];
+// 	    }
+// 	    int ans= n ;
+// 	    for(int i =1 ; i*i<=n ; i++){
+// 	        ans  = min(ans,1+solveMemoisation(n- i*i,dp)) ;
+// 	    }
+// 	     dp[n] = ans ;
+// 	     return dp[n] ;
+// 	}
+	int solveTab(int n,vector<int>&dp){
+	    dp[0] = 0 ;
+	    for(int i = 1 ; i<=n ; i++){
+	        for(int j = 1 ; j*j<=n ;j++){
+	            if(i-j*j>=0)
+	            dp[i]  = min(dp[i],1+dp[i-j*j]) ;
+	        }
 	    }
-	    if(dp[n]!=-1){
-	        return dp[n];
-	    }
-	    int ans= n ;
-	    for(int i =1 ; i*i<=n ; i++){
-	        ans  = min(ans,1+solveMemoisation(n- i*i,dp)) ;
-	    }
-	     dp[n] = ans ;
-	     return dp[n] ;
+	    return dp[n];
 	}
 	int MinSquares(int n)
 	{
-	    vector<int>dp(n+1,-1) ;
-	    return  solveMemoisation(n,dp) ;
+	    vector<int>dp(n+1,INT_MAX) ;
+	    return  solveTab(n,dp) ;
 	}
 };
 
