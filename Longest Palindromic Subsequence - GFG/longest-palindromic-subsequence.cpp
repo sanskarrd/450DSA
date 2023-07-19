4b3f6b38b3cfa5ce52799a73f8460a7874e7c8f9
Sanskar Dubey <91963143+sanskarrd@users.noreply.github.com>
2023-07-19 20:03:46 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -8,45 +8,29 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution {
-public:
-    //  int solve(int i , int j ,string s1,string s2,vector<vector<int>>&dp){
-    //      if(i==0 || j==0){
-    //          return 0 ;
-    //      }
-    //      if(dp[i][j] != -1){
-    //          return dp[i][j] ;
-    //      }
-    //      if(s1[i-1] == s2[j-1]){
-    //          return 1+ solve(i-1,j-1,s1,s2,dp) ;
-    //      }
+class Solution{
+  public:
+  int solve(int i , int j ,string s1,string s2,vector<vector<int>>&dp){
+         if(i==0 || j==0){
+             return 0 ;
+         }
+         if(dp[i][j] != -1){
+             return dp[i][j] ;
+         }
+         if(s1[i-1] == s2[j-1]){
+             return 1+ solve(i-1,j-1,s1,s2,dp) ;
+         }
          
-    //      return dp[i][j] = max(solve(i-1,j,s1,s2,dp) , solve(i,j-1,s1,s2,dp)) ;
-    //  }
+         return dp[i][j] = max(solve(i-1,j,s1,s2,dp) , solve(i,j-1,s1,s2,dp)) ;
+     }
     int longestPalinSubseq(string A) {
         int n = A.size() ; 
         string B  = A;
         reverse(B.begin(),B.end());
         vector<vector<int>>dp(n+1,vector<int>(n+1,-1)) ;
-        
-        
-        for(int i = 0 ; i<=n ; i++) dp[i][0] = 0 ;
-        for(int j = 0 ; j<=n ; j++) dp[0][j] = 0 ; 
-        
-        for(int i = 1 ; i<=n ; i++){
-            for(int j = 1 ; j<= n ; j++){
-                if(A[i-1] == B[j-1]){
-                    dp[i][j]  = 1 + dp[i-1][j-1] ;
-                }
-                else{
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]) ;
-                }
-            }
-        }
-        return dp[n][n] ;
+        return solve(n,n,A,B,dp) ;
     }
 };
-
 
 //{ Driver Code Starts.
 
